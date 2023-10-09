5644429f9697b876fb668ccf498f6d2558684bf2
ernane junior <ernanecdj44@gmail.com>
2023-10-09 16:10:27 -0400

    arquivo script e texto para sobre mim adicionados

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function sobreMim(){
+    var pontos=document.getElementById("pontos");
+    var maisTexto=document.getElementById("mais");
+    var btnLeiaMais=document.getElementById("btnLeiaMais");
+
+    if(pontos.style.display === "none"){
+        pontos.style.display="inline";
+        maisTexto.style.display="none";
+    }else{
+        pontos.style.display="none";
+        maisTexto.style.display="inline";
+    }
+}
